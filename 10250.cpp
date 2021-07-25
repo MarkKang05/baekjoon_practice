7771272b76173ac19354d886e2ff5495ebc848b7
MarkKang05 <markkang05@gmail.com>
2021-07-25 23:08:19 +0900

    210725

--- 10250.cpp
+++ 10250.cpp
@@ -5,24 +5,33 @@ int main(){
     int t,h,w,n;
 
     cin >> t;
-
-    for(int i=0; i<t; i++){
+    for(int i=0; i<t;i++){
         cin >> h >> w >> n;
-
-        if(n/h%10!=0){
-            if(n%h!=0){
-                cout<<n%h<< 0 <<n/h+1;
-                continue;
-            }
-            cout<<n%h+1<< 0 <<n/h+1;
-            continue;
-        }
-        if(n%h!=0){
-            cout<<n%h<<n/h+1;
-            continue;
-        }
-        cout<<n%h+1<<n/h+1;
+        int hh, ww;
+        hh = n%h;
+        ww = n/h;
+        if(hh > 0) ww++;
+        else hh = h;
+        cout << hh*100 + ww << "\n";
     }
+
+/*     for(int i=0; i<t; i++){
+ *         cin >> h >> w >> n;
+ *
+ *         if(n/h%10!=0){
+ *             if(n%h!=0){
+ *                 cout<<n%h<< 0 <<n/h+1;
+ *                 continue;
+ *             }
+ *             cout<<n%h+1<< 0 <<n/h+1;
+ *             continue;
+ *         }
+ *         if(n%h!=0){
+ *             cout<<n%h<<n/h+1;
+ *             continue;
+ *         }
+ *         cout<<n%h+1<<n/h+1;
+ *     } */
 
 }
 
